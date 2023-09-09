408692688e508eb1a081195e8326189f1a8eab13
770886dt <144524391+770886dt@users.noreply.github.com>
2023-09-09 15:10:35 +0530

    Add files via upload

---
+++ challenge-unit1/main.py
@@ -0,0 +1,24 @@
+def fact_rec(n):
+  if n==0orn==1:
+  return 1
+else:
+     return n*fact(n-1)
+  number=int(input("enter the number:"))
+res=fact_res(number)
+print("the factorial of{}is{},"format(number,res))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+8
